fd52bd62230f554e32ecba4e01bc4ab1adf75c6e
Kirill Klimuk <kklimuk@ebay.com>
2015-02-24 00:01:24 -0800

    break zips up into smaller files

---
+++ breaker.py
@@ -0,0 +1,11 @@
+from sys import stdin, argv
+import json
+
+# hook this up as a unix utility:
+# reads from STDIN and writes to STDOUT
+if __name__ == '__main__':
+	with stdin as standard_input:
+		data = json.loads(stdin.read())['features']
+		for index, i in enumerate(xrange(0, len(data), 5000)):
+			with file(argv[1] + str(index) +  '.json', 'w+') as current:
+				current.write(json.dumps(data[i:i+5000]))
